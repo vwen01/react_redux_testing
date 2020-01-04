9e3ecba98fd578304b922b5e580fa8fdd5a7bb35
vwen01 <v.wen01@gmail.com>
2020-01-04 13:05:00 +1100

    Create header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Header from "./components/header/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      React Redux Thunk Test
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Header/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
